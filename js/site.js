1efacce34ad57aa4396ce9215042469e76143b80
Andrew Russell <67603030+ARussell-CF@users.noreply.github.com>
2021-04-14 11:53:36 -0400

    first commit

---
+++ js/site.js
@@ -0,0 +1,66 @@
+//contains 126 fruits all starting with lower case letters
+const fruits = ["abiu", "açaí", "acerola", "ackee", "african cucumber", "apple", "apricot", "avocado", "banana", "bilberry", "blackberry", "blackcurrant", "black sapote", "blueberry", "boysenberry", "breadfruit", "buddha's hand", "cactus pear", "canistel", "cempedak", "crab apple", "currant", "cherry", "cherimoya", "chico fruit", "cloudberry", "coco de mer", "coconut", "cranberry", "damson", "date", "dragonfruit", "durian", "egg Fruit", "elderberry", "feijoa", "fig", "goji berry", "gooseberry", "grape", "grewia asiatica", "raisin", "grapefruit", "guava", "hala fruit", "honeyberry", "huckleberry", "jabuticaba", "jackfruit", "jambul", "japanese plum", "jostaberry", "jujube", "juniper berry", "kaffir Lime", "kiwano", "kiwifruit", "kumquat", "lemon", "lime", "loganberry", "loquat", "longan", "lulo", "lychee", "magellan barberry", "mamey apple", "mamey sapote", "mango", "mangosteen", "marionberry", "melon", "cantaloupe", "galia melon", "honeydew", "mouse melon", "watermelon", "miracle fruit", "monstera deliciosa", "mulberry", "nance", "nectarine", "orange", "blood orange", "clementine", "mandarine", "tangerine", "papaya", "passion fruit", "peach", "pear", "persimmon", "plantain", "plum", "prune", "pineapple", "pineberry", "plumcot", "pomegranate", "pomelo", "purple mangosteen", "quince", "raspberry", "salmonberry", "rambutan", "redcurrant", "rose apple", "salal berry", "salak", "satsuma", "shine muscat", "sloe berry", "soursop", "star apple", "star fruit", "strawberry", "surinam cherry", "tamarillo", "tamarind", "tangelo", "tayberry", "tomato", "ugli fruit", "white currant", "white sapote", "yuzu"];
+
+function checkStand() {
+    //Gather the user's input
+    let fruitOne = document.getElementById("fruitOne").value;
+    let fruitTwo = document.getElementById("fruitTwo").value;
+    let fruitThree = document.getElementById("fruitThree").value;
+
+    //check that there is at least one letter in the input
+    if (fruitOne.length > 0) {
+        //normalize the input to lower case
+        let check = fruitOne.toLowerCase().trim();
+        //use the .includes() function to check the array
+        if (fruits.includes(check)) {
+            //if the fruit was found output the location
+            let boxNum = fruits.indexOf(check) + 1;
+            let output = document.getElementById("outputOne");
+            output.innerText = `We found ${fruitOne} in box number ${boxNum}`;
+        } else {
+            let output = document.getElementById("outputOne");
+            output.innerText = `Looks like we are out of ${fruitOne}`;
+        }
+    } else {
+        //In case the user doesn't use the first box, but does use the other two
+        let output = document.getElementById("outputOne");
+        output.innerText = "You didn't use the first search box";
+    }
+    if (fruitTwo.length > 0) {
+        //normalize the input to lower case
+        let check = fruitTwo.toLowerCase().trim();
+        //use the .includes() function to check the array
+        if (fruits.includes(check)) {
+            //if the fruit was found output the location
+            let boxNum = fruits.indexOf(check) + 1;
+            let output = document.getElementById("outputTwo");
+            output.innerText = `We found ${fruitTwo} in box number ${boxNum}`;
+        } else {
+            let output = document.getElementById("outputTwo");
+            output.innerText = `Looks like we are out of ${fruitTwo}`;
+        }
+    }
+    if (fruitThree.length > 0) {
+        //normalize the input to lower case
+        let check = fruitThree.toLowerCase().trim();
+        //use the .includes() function to check the array
+        if (fruits.includes(check)) {
+            //if the fruit was found output the location
+            let boxNum = fruits.indexOf(check) + 1;
+            let output = document.getElementById("outputThree");
+            output.innerText = `We found ${fruitThree} in box number ${boxNum}`;
+        } else {
+            let output = document.getElementById("outputThree");
+            output.innerText = `Looks like we are out of ${fruitThree}`;
+        }
+    }
+}
+
+function clearForm() {
+    let form = document.getElementById("checkForm");
+    form.reset();
+}
+
+function allFruits() {
+
+}
